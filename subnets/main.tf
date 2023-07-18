2b3bfd9b70b7b2176f0309793a63b4b037d2668e
sreesai169 <sreesai169@gmail.com>
2023-07-18 10:33:18 +0530

    Committing terraform changes for creating instances

--- subnets/main.tf
+++ subnets/main.tf
@@ -3,6 +3,6 @@ resource "aws_subnet" "main" {
   vpc_id = var.vpc_id
   cidr_block = var.cidr_block[count.index]
   tags = {
-    Name = merge(var.tags,{ Name = "${var.env}-${var.name}-${count.index}"})
+    Name = merge(var.tags,{ Name = "${var.env}-${var.name}"})
   }
 }
